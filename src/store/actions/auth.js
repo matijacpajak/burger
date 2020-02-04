ab18039913a3e0f1a9d108df2dde1ebaad7d16e0
matijacpajak <matijacpajak@gmail.com>
2020-02-04 19:06:39 +0100

    Sign in/Sign up

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,7 +22,7 @@ export const authFail = ( error ) => {
     };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
@@ -30,7 +30,11 @@ export const auth = (email, password) => {
             password: password,
             returnSecureToken: true
         }
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA11ToVxoYXsdBUuY_cx3krThryxpHXZKk', authData)
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA11ToVxoYXsdBUuY_cx3krThryxpHXZKk';
+        if(!isSignup) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA11ToVxoYXsdBUuY_cx3krThryxpHXZKk';
+        }
+        axios.post(url, authData)
             .then(response => {
                 console.log(response);
                 dispatch(authSuccess(response.data));
